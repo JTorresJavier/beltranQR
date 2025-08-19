9f4d907b831ee7e7384c35cf38e07863c12a6090
Sliv <franco.ngrodriguez@gmail.com>
2025-08-19 14:41:37 -0300

    Se ha agregado el código js correspondiente para generar QRs Se ha agregado el código js correspondiente del menu hamburguesa Se ha modificado el código el HTML y CSS del generador qr

---
+++ JavaScript/GenerarQR.js
@@ -0,0 +1,21 @@
+const qrContainer = document.getElementById("qrcode");
+const textoQR = document.getElementById("textoQR");
+
+let QR;
+
+function generarQR() {
+    const Valor = Math.random().toString(36).substring(2, 10);
+    const codigoCompleto = "Usuario-Entrada/Salida-" + Valor;
+    if (QR) {
+        QR.clear();
+    } else {
+        QR = new QRCode(qrContainer, {
+          width: 230,
+          height: 230,
+          correctLevel: QRCode.CorrectLevel.H 
+        });
+    }
+    QR.makeCode(codigoCompleto);
+    qrContainer.style.display = "block";
+    textoQR.textContent = `Acerca el código QR al lector para escanear`;
+}
---
+++ JavaScript/Hamburguesa.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const sideMenu = document.getElementById("sideMenu");
+  const overlay = document.getElementById("menuOverlay");
+
+  hamburger.addEventListener("click", () => {
+    sideMenu.classList.toggle("open");
+    overlay.classList.toggle("active");
+  });
+
+  overlay.addEventListener("click", () => {
+    sideMenu.classList.remove("open");
+    overlay.classList.remove("active");
+  });
+});
